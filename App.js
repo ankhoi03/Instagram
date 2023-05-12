58bbeb63f9aadc7e4b4c40eb3b7ef533fdcfca66
trieudeptrai03 <117624810+trieudeptrai03@users.noreply.github.com>
2023-05-13 00:33:50 +0700

    Test

--- App.js
+++ App.js
@@ -3,19 +3,27 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow strict-local
  */
 
 import React from 'react';
+import { View,StyleSheet, Image, Text } from 'react-native';
+import Register from './src/components/app/user/screens/Register';
+import Login from './src/components/app/user/screens/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import  AppNavigation from './src/components/app/navigation/AppNavigation'
+import  {UserProvider} from  './src/components/app/user/utilities/UserContext'
+import  {NewsProvider} from  './src/components/app/news/utilities/NewsContext'
+import Signin from './src/Signin';
+
+
+const App = () => {
+  return (
+      <></>
+  );
+}
+
 
 export default App;
